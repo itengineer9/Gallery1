701b52654e289da019547dfff91570b5b1f65ebb
User <itengineer9@yahoo.com>
2021-04-27 16:33:18 +0200

    Gallery #1

---
+++ js/main.js
@@ -0,0 +1,65 @@
+
+$(function(){
+    
+    $('nav a').on('click', function (e) {
+        e.preventDefault();
+        $('h1#heading2').text($(this).text());
+        
+        var art = $(this).text().toLowerCase();
+        
+        if(art === 'alle_bilder'){
+            loadImages("js/allBilder.json");
+        }else if(art === 'autos'){
+           loadImages("js/autos.json");
+        }else if(art === 'fahrraeder'){
+            loadImages("js/fahrraerder.json");
+        }else if(art === 'flugzeuge'){
+            loadImages("js/flugzeuge.json");
+        }           
+        return false;
+    });
+    
+   // Add Img to the Extended Img
+    $('#images').on('click','img', function () {       
+        var currntImg = $(this).attr('src'); 
+        $('.extended').fadeIn(1000);
+        $('.extended img').attr('src', currntImg);
+    });
+    
+    
+    // close button
+    $('.close').on('click', function () {   
+        $(this).parent().toggle(1000);
+    });
+       
+   
+}); //END Ready
+
+
+/**
+ * load Fahrräder Bilder mit Ajax / json 
+ * @param {string} file path to json file
+ * @returns {undefined}
+ */
+function loadImages(file){
+        $.ajaxSetup({cache: false});
+        var $file = file;
+        $.getJSON($file)
+            .done(function (Bilder) {
+                $('#images').empty();
+                $.each(Bilder, function (key) {
+                    let bild = Bilder[key].surc;
+                     let txt = Bilder[key].alt;
+                    $(`<img src=${bild} alt =${txt}></img>`)
+                            .appendTo('#images').fadeIn('slow');
+                    
+                });
+            })
+            
+            .fail(function () {
+                $('h1#heading2').text('Entschuldigung ,,,, etwas Passiert');
+            })
+           ;
+}
+
+ 
